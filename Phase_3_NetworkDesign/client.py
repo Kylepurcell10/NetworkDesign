0d596646da1ce4eb3b909780cf14fded69c71607
KyleLuong227 <kyleluong227@gmail.com>
2021-03-24 12:50:08 -0400

    dataObject to just data

--- Phase_3_NetworkDesign/client.py
+++ Phase_3_NetworkDesign/client.py
@@ -24,26 +24,26 @@ currentSequence = 0
 # packet acknowledgement
 currentACK = 0
 # File to be sent over the network, this can be changed to any of the .bmp files that are included
-filename = '/src/Projects/Network Design/Network Design Phases/NetworkDesign/Phase_3_NetworkDesign\trashbin.jpg'
+filename = '/src/Projects/Network_Design/Network_Design_Phases/NetworkDesign/Phase_3_NetworkDesign/trashbin.jpg'
 # Open the file and begin reading it with packet size of 1024
 file = open(filename , 'rb')
 # current data item being processed
 data = file.read(bufferSize)
 
-print("UDP target IP:", IP)
-print("UDP target port:", Port)
+print("UDP IP:", IP)
+print("UDP port:", Port)
 
 
-def rdtSend(dataObject):
+def rdtSend(data):
     global currentSequence, currentACK
 
     # Create the Checksum
-    values = (currentACK, currentSequence, base64.b64encode(dataObject.encode('utf-8')))
+    values = (currentACK, currentSequence, base64.b64encode(data.encode('utf-8')))
     UDPData = struct.Struct('I I 8s')
     packedData = UDPData.pack(*values)
     checksumVal = hashlib.md5(packedData).hexdigest().encode('utf-8')
     # gets the constructed UDP packet
-    sendPacket = makepacket(currentACK, dataObject, checksumVal)
+    sendPacket = makepacket(currentACK, data, checksumVal)
     # send the UDP packet
     UDPSend(sendPacket)
 
@@ -97,7 +97,7 @@ def isACK(receivePacket, ACKVal):
 def rdtReceive(receivePacket):
     global currentACK
     global currentSequence
-    global dataObject
+    global data
 
     # if the packet does not have an error and is acknowledged, then adjust variables for next packet
     # otherwise resend packet
@@ -108,7 +108,7 @@ def rdtReceive(receivePacket):
     else:
         # packet error or no acknowledgement, resend the previous packet
         print('Invalid packet: ', receivePacket)
-        rdtSend(dataObject)
+        rdtSend(data)
         return False
 
 
@@ -117,14 +117,14 @@ dataList = ['trashbin.jpg']
 #In this case the JPEG image is used 
 
 # send the data items in data list consequetively
-for dataObject in dataList:
+for data in dataList:
     success = False
 
     # send the data item until both sequences have been acknowledged by the server (success == True)
     while success == False:
         # send the data item
-        rdtSend('b' + dataObject)
-        print('Data sent: ', dataObject)
+        rdtSend('b' + data)
+        print('Data sent: ', data)
 
         # Receive Data
         try:
