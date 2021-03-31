0bbd42b404a23d9bfb066e65f6c0d8ca7a538647
KyleLuong227 <kyleluong227@gmail.com>
2021-03-31 15:30:44 -0400

    Finally

--- Phase4_Attempt/Our Attempt/Functions.py
+++ Phase4_Attempt/Our Attempt/Functions.py
@@ -3,10 +3,16 @@ import math
 import random
 import struct
 import time
-from Globals import*
+
+start = time.time()                    #This is used to find the start time of the program, elapsed time can be found by end time - start time
+
+IP = "127.0.0.1"                   #Localhost is the IP address of this machine
+Port = 20001                       #Port Number is assigned to 5005
+bufferSize = 1024                     #bufferSize is set to 1024. packet size is 1024 with sequence number 1 byte, checksum 2 bytes, data 1021 bytes.
+addr = (IP,Port)
 
 '''This function is used to find the file size with the help of seek function.'''
-def File_size(file):
+def fileSize(file):
     file.seek(0,2)                                                  # moves the file pointer to the EOF
     fileSize = file.tell()                                          #gets file size
     file.seek(0,0)                                                  # moves the file pointer the the beginning of the file
@@ -14,55 +20,55 @@ def File_size(file):
 
 
 '''This function is used to find how many loops the program has to run to transfer the file.'''
-def looptimes(fileSize, buffer_size):
-    loopTimes = (fileSize / (buffer_size-3))                        #Filesize is divided by (buffersize (1024)-3) to find the loopTimes, because 3 bytes will be the headers for the packet
+def looptimes(fileSize, bufferSize):
+    loopTimes = (fileSize / (bufferSize - 3))                        #Filesize is divided by (buffersize (1024)-3) to find the loopTimes, because 3 bytes will be the headers for the packet
     loop = math.ceil(loopTimes)                                     #Changing loopTimes to next integer
     return (loop)                                                   #returns the loop value in integer
 
 
 '''This function updates the sequence number'''
-def Update_Seq_num(seq_num):
-    return 1-seq_num                                                #returns 1-seq_num in integer
+def updateSeqNum(seqNum):
+    return 1 - seqNum                                                #returns 1-seqNum in integer
 
 
 '''This function is used to find the Checksum for the data'''
 def checksum(data):
-    checksum_addition=0                                             #inital checksum value is zero
-    for i in range(0,len(data),2):                                  #Loop starts from 0 to len(data)-1, iterated +2 times.
-        first_2bits = data[i : (i+2)]                               #taking 16 bits (2 bytes) value from 1024 bytes
-        if len(first_2bits) == 1:
-            two_byte_integer = struct.unpack("!B",first_2bits)[0]   #If len(data)=1 it has to be unpacked with standard size 1
-        elif len(first_2bits) == 2:
-            two_byte_integer = struct.unpack("!H",first_2bits)[0]   #If len(data)=2 it has to be unpacked with standard size 2
-        checksum_addition = checksum_addition + two_byte_integer    #checksum addition
-        while (checksum_addition>>16)==1:                           #loop goes on until condition becomes 'false'
-            checksum_addition = (checksum_addition & 0xffff) + (checksum_addition >>16)  #Wrapup function
-    return checksum_addition                                        #returns checksum for the data in integer
+    checksumAdd=0                                             #inital checksum value is zero
+    for i in range(0, len(data), 2):                                  #Loop starts from 0 to len(data)-1, iterated +2 times.
+        firstTwoBits = data[i : (i + 2)]                               #taking 16 bits (2 bytes) value from 1024 bytes
+        if len(firstTwoBits) == 1:
+            twoByteInt = struct.unpack("!B",firstTwoBits)[0]   #If len(data)=1 it has to be unpacked with standard size 1
+        elif len(firstTwoBits) == 2:
+            twoByteInt = struct.unpack("!H",firstTwoBits)[0]   #If len(data)=2 it has to be unpacked with standard size 2
+        checksumAdd = checksumAdd + twoByteInt    #checksum addition
+        while (checksumAdd >> 16) == 1:                           #loop goes on until condition becomes 'false'
+            checksumAdd = (checksumAdd & 0xffff) + (checksumAdd >>16)  #Wrapup function
+    return checksumAdd                                        #returns checksum for the data in integer
 
 
 '''This function is used to find the Bit_Error has to happen or not'''
-def Error_Condition(E_Prob=0):
-    Data_Bit_Error = False                                          #Data_Bit_Error has been initialised as 'False'
-    Random_Num = random.random()                                    #This generates a random probability value (0.00 to 1.00)
-    if (Random_Num < (E_Prob/100)):                                 #converting percentage(E_Prob) to probability [(0 to 100) into (0.00 to 1.00)] in order to compare with Random_Num
-        Data_Bit_Error = True                                       #If condition is 'True' it corrupts data
-    return Data_Bit_Error                                           #returns Data_Bit_Error as 'True' or 'False'
+def errorCondition(packetErrorProbability = 0):
+    dataBitError = False                                          #dataBitError has been initialised as 'False'
+    randNum = random.random()                                    #This generates a random probability value (0.00 to 1.00)
+    if (randNum < (packetErrorProbability / 100)):                                 #converting percentage(packetErrorProbability) to probability [(0 to 100) into (0.00 to 1.00)] in order to compare with randNum
+        dataBitError = True                                       #If condition is 'True' it corrupts data
+    return dataBitError                                           #returns dataBitError as 'True' or 'False'
 
 
 '''This Function is used to corrupt the data'''
-def Data_Corrupt(data):
+def corruptData(data):
     return (b'XX'+ data[2:])                                        #Replacing the first two bytes of data with alphabet character 'X' in order to corrupt, returns in byte
 
 
 '''This Function is used to Extract Data (Sequence number,checksum, data) from packet'''
-def ExtractData(packet):                                            #Extracts the packet
-    dataLen=len(packet)-len("!BH")                                  #this is used to find the length of the data, (length of the sequence number (1byte) and checksum(2bytes) are fixed)
-    pktFrmt="!BH"+str(dataLen)+"s"                                  #This is the packet format. example if data length is 1021 bytes then it should be "!BH1021s".
-    return struct.unpack(pktFrmt,packet)                            #returns the unpacked values of packet.
+def extractData(packet):                                            #Extracts the packet
+    dataLen = len(packet) - len("!BH")                                  #this is used to find the length of the data, (length of the sequence number (1byte) and checksum(2bytes) are fixed)
+    packetFormat = "!BH"+str(dataLen) + "s"                                  #This is the packet format. example if data length is 1021 bytes then it should be "!BH1021s".
+    return struct.unpack(packetFormat,packet)                            #returns the unpacked values of packet.
 
 
 '''This Function is used to make packet (Sequence number + checksum + data -> together forms a packet)'''
-def MakePkt(seqNums,chksums,data):
-    pktFrmt="!BH"+str(len(data))+"s"                                #This is the packet format. example if data length is 1021 bytes then it should be "!BH1021s".
-    packet=struct.pack(pktFrmt,seqNums,chksums,data)                #Packs sequence number, checksum,data and forms a packet
+def makePacket(seqNums,chksums,data):
+    packetFormat = "!BH" + str(len(data)) + "s"                                #This is the packet format. example if data length is 1021 bytes then it should be "!BH1021s".
+    packet = struct.pack(packetFormat,seqNums,chksums,data)                #Packs sequence number, checksum,data and forms a packet
     return packet                                                   #returns packet in bytes
--- Phase4_Attempt/Our Attempt/server.py
+++ Phase4_Attempt/Our Attempt/server.py
@@ -4,82 +4,86 @@ import random
 import struct
 import time
 import Functions
-from Globals import*
-import Send_Receive
+
+start = time.time()                    #This is used to find the start time of the program, elapsed time can be found by end time - start time
+
+IP = "127.0.0.1"                   #Localhost is the IP address of this machine
+Port = 20001                       #Port Number is assigned to 5005
+bufferSize = 1024                     #bufferSize is set to 1024. packet size is 1024 with sequence number 1 byte, checksum 2 bytes, data 1021 bytes.
+addr = (IP,Port)
 
 
-def RdtReceivePkt (sock,buffer_size,Rx_seq_num,E_Prob=0,P_Drop=0):
-    receive_successful = 0                                                                  #Receive_sucessful is set to '0' initially
-    while (not(receive_successful)):                                                        #loop goes on until condition becomes 'false'
+def rdtReceivePacket (serverSocket,bufferSize,receiveSeqNum,packetErrorProbability=0,packetDropProbability=0):
+    successfulReceive = 0                                                                  #Receive_sucessful is set to '0' initially
+    while (not(successfulReceive)):                                                        #loop goes on until condition becomes 'false'
 
-        data, address = sock.recvfrom(buffer_size)                                          #Packet is received from client
+        data, address = serverSocket.recvfrom(bufferSize)                                          #Packet is received from client
 
-        if (Functions.Error_Condition(P_Drop)):                                             #If Data_bit_error is true, it starts to Drop packet intentionally ! by coming out of while-loop. Basically The received packet not utilised/used.
-          print ("DATA PACKET DROPPED\n")
-          receive_successful=0                                                              #Comes out of current loop and starts again since condition will be while(1).
+        if (Functions.errorCondition(packetDropProbability)):                                             #If dataBitError is true, it starts to Drop packet intentionally ! by coming out of while-loop. Basically The received packet not utilised/used.
+          print ("Data Packet Dropped\n")
+          successfulReceive = 0                                                              #Comes out of current loop and starts again since condition will be while(1).
 
-        else:                                                                               #If Data_bit_error is False,then it refers to No-packet dropping. It goes to else loop and utilises the received packet.
-            seq_num,checksum,Img_Pkt=Functions.ExtractData(data)                            #Extracts the sequence number, checksum value, data from a packet
+        else:                                                                               #If dataBitError is False,then it refers to No-packet dropping. It goes to else loop and utilises the received packet.
+            seqNum,checksum,imagePacket=Functions.extractData(data)                            #Extracts the sequence number, checksum value, data from a packet
 
-            if (Functions.Error_Condition(E_Prob)):                                         #If Data_bit_error is true, it starts to corrupt packet intentionally
-                Img_Pkt = Functions.Data_Corrupt(Img_Pkt)                                   #Function to corrupt data
+            if (Functions.errorCondition(packetErrorProbability)):                                         #If dataBitError is true, it starts to corrupt packet intentionally
+                imagePacket = Functions.corruptData(imagePacket)                                   #Function to corrupt data
                 print ("\nData Corrupted")
 
 
-            Rx_checksum = Functions.checksum(Img_Pkt)                                       #Receiver Checksum in integer
+            receiverChecksum = Functions.checksum(imagePacket)                                       #Receiver Checksum in integer
 
-            if ((Rx_checksum == checksum) and (seq_num == Rx_seq_num)):                     #if packet is not corrupted and has expected sequence number, sends Acknowledgement with sequence number  *updates sequence number for next loop
-                    Ack = Rx_seq_num                                                        #sends sequence number
+            if ((receiverChecksum == checksum) and (seqNum == receiveSeqNum)):                     #if packet is not corrupted and has expected sequence number, sends Acknowledgement with sequence number  *updates sequence number for next loop
+                    Ack = receiveSeqNum                                                        #sends sequence number
                     Ack = b'ACK' + str(Ack).encode("UTF-8")                                 #Converting (Ack) from int to string and then encoding to bytes
-                    Sender_Ack = Functions.MakePkt(seq_num,Functions.checksum(Ack),Ack)     #Server sends Ack with Seq_num, checksum, Ack
-                    print("Sequence Number: {0},Receiver_sequence: {1}, Checksum from Client: {2}, Checksum for Received File: {3}\n".format(seq_num,Rx_seq_num,checksum,Rx_checksum))
-                    Rx_seq_num = 1-seq_num                                                  #update sequence number to the next expected seqNum
-                    receive_successful = 1                                                  #Comes out of while loop
+                    senderACK = Functions.makePacket(seqNum,Functions.checksum(Ack),Ack)     #Server sends Ack with Seq_num, checksum, Ack
+                    print("Sequence #: {0}, Receiver Sequence: {1}, Checksum from Client: {2}, Checksum for Received File: {3}\n".format(seqNum,receiveSeqNum,checksum,receiverChecksum))
+                    receiveSeqNum = 1 - seqNum                                                  #update sequence number to the next expected seqNum
+                    successfulReceive = 1                                                  #Comes out of while loop
 
-            elif ((Rx_checksum != checksum) or (seq_num != Rx_seq_num)):                    #if packet is corrupted or has unexpected sequence number, sends Acknowledgement with previous Ackowledged sequence number. Requests client to resend the data.
-                    Ack = 1 - Rx_seq_num                                                    #last acked sequence numvber
+            elif ((receiverChecksum != checksum) or (seqNum != receiveSeqNum)):                    #if packet is corrupted or has unexpected sequence number, sends Acknowledgement with previous Ackowledged sequence number. Requests client to resend the data.
+                    Ack = 1 - receiveSeqNum                                                    #last acked sequence numvber
                     Ack = b'ACK' + str(Ack).encode("UTF-8")                                 #Converting (Ack) from int to string and then encoding to bytes
-                    Sender_Ack = Functions.MakePkt(1 - Rx_seq_num,Functions.checksum(Ack),Ack) #Server sends Ack with Seq_num, checksum, Ack
+                    senderACK = Functions.makePacket(1 - receiveSeqNum,Functions.checksum(Ack),Ack) #Server sends Ack with Seq_num, checksum, Ack
                     print("Server Requested to Resend the Packet")
-                    print("Sequence Number: {0},Receiver_sequence: {1}, Checksum from Client: {2}, Checksum for Received File: {3}\n".format(seq_num,Rx_seq_num,checksum,Rx_checksum))
-                    receive_successful = 0                                                  #Loop continues until satisfies condition
-            sock.sendto(Sender_Ack,address)                                                 #sending the Acknowledgement packet to the client
+                    print("Sequence #: {0}, Receiver Sequence: {1}, Checksum from Client: {2}, Checksum for Received File: {3}\n".format(seqNum,receiveSeqNum,checksum,receiverChecksum))
+                    successfulReceive = 0                                                  #Loop continues until satisfies condition
+            serverSocket.sendto(senderACK,address)                                                 #sending the Acknowledgement packet to the client
 
-    return Img_Pkt,address,Rx_seq_num
+    return imagePacket,address,receiveSeqNum
 
-'''To corrupt acknowledgemnt packet, Set value from 0 - 99 in E_prob'''
-packetErrorProbability = 0                                                  #E_Prob is the error probability and can be set from 0-99
-packetDropProbability = 0                                                   #P_Drop is the packet dropping probability and can be set from 0-99
+#To corrupt acknowledgemnt packet, Set value from 0 - 99 in packetErrorProbability
+packetErrorProbability = 0                                                  #packetErrorProbability is the error probability and can be set from 0-99
+packetDropProbability = 0                                                   #packetDropProbability is the packet dropping probability and can be set from 0-99
 
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)                      #Socket with IPV4, UDP
-sock.bind(addr)                                                             #Binding the socket
-print("Server Started")
-fileName = 'Received_Image.jpg'
+serverSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)                      #Socket with IPV4, UDP
+serverSocket.bind(addr)                                                             #Binding the socket
+print("Server Ready to Receive")
+fileName = '/src/Projects/Network_Design/Network_Design_Phases/NetworkDesign/Phase4_Attempt/Our Attempt/Received_Image.jpg'
 file = open(fileName, 'wb')                                                 #opening a new file to copy the transferred image
 
-receiver_sequence = 0                                                       #Server side Sequence number is initialised to zero
-loopTimes,address,receiver_sequence = Send_Receive.RdtReceivePkt(sock,buffer_size,receiver_sequence) #Receiving the file size from client
-loop= struct.unpack("!I", loopTimes)[0]                                     #changing loop from byte to integer
+receiverSequence = 0                                                       #Server side Sequence number is initialised to zero
+loopTimes,address,receiverSequence = rdtReceivePacket(serverSocket,bufferSize,receiverSequence) #Receiving the file size from client
+loop = struct.unpack("!I", loopTimes)[0]                                     #changing loop from byte to integer
 print ("No. of Loops to send the entire file: ", loop)
-print("write/Receiving process starting soon")                              #Receiving File from Client
+print("Writing/Receiving process starting soon")                              #Receiving File from Client
 
 for i in range(0,loop):                                                     #Loop to write entire transferred image in the new file, it runs 'loop' times
-    print("Loop :",i+1)                                                     #Prints the Current loop. For easier way, initial print value starts from 1 (i+1, where i is 0)
-    if(i>= (loop-2)):                                                       #This is used to make sure corruption is not made at last loop, if not client or server keeps on waiting for ack/data.
-        packetErrorProbability=0                                                            #Error probability manually set to zero (No corruption) if true.
-        packetDropProbability=0                                                            #Packet Dropping probability manually set to zero (No corruption) if true.
-    packet, address, receiver_sequence = Send_Receive.RdtReceivePkt(sock,buffer_size,receiver_sequence,packetErrorProbability,packetDropProbability)   #Calls the function RdtReceivePkt to receive the packet
-    #Change to just RdtSendpkt
+    print("Loop #:", i + 1)                                                     #Prints the Current loop. For easier way, initial print value starts from 1 (i+1, where i is 0)
+    if(i >= (loop - 2)):                                                       #This is used to make sure corruption is not made at last loop, if not client or server keeps on waiting for ack/data.
+        packetErrorProbability = 0                                                            #Error probability manually set to zero (No corruption) if true.
+        packetDropProbability = 0                                                            #Packet Dropping probability manually set to zero (No corruption) if true.
+    packet, address, receiverSequence = rdtReceivePacket(serverSocket,bufferSize,receiverSequence,packetErrorProbability,packetDropProbability)   #Calls the function rdtReceivePacket to receive the packet
     file.write(packet)                                                      #If packet received successful, It writes to the file
-    i=i+1                                                                   #Loop Iteration
+    i = i + 1                                                                   #Loop Iteration
 #File Received from Client at the end of Loop
 
-Received_File_Size = Functions.File_size(file)                                 #Calculating Received Image file size
+receivedFileSize = Functions.fileSize(file)                                 #Calculating Received Image file size
 
 file.close()                                                                #closing the file
-sock.close()                                                                #closing the socket
+serverSocket.close()                                                                #closing the socket
 
 end = time.time()                                                           #Finding the end-time
-Elapsed_time = end -  start                                                 #Elapsed time
-print ("Server: File Received\nReceived File size: {0}\nTime taken in Seconds: {1}s".format(Received_File_Size,Elapsed_time))
+elapsedTime = end - start                                                 #Elapsed time
+print ("Server: File Received\nReceived File size: {0}\nTime taken in Seconds: {1}".format(receivedFileSize,elapsedTime))
 
